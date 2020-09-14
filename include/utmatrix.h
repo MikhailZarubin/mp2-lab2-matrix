1549af9afcd4e45ec6a0adc3e138e8c5ff80f58b
vasya <e.semenuta@yandex.ru>
2020-09-14 16:54:51 +0300

    first commot 2020

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -62,6 +62,12 @@ public:
 template <class ValType>
 TVector<ValType>::TVector(int s, int si)
 {
+    if (s <= 0 || s > MAX_MATRIX_SIZE)
+    {
+        throw s;
+    }
+    size = s;
+    pVector = new ValType[size];
 } /*-------------------------------------------------------------------------*/
 
 template <class ValType> //конструктор копирования
